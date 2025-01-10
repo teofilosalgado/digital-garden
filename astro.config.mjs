1b90be94ba89d7f40e1b29134d6847f72e073a98
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2025-01-10 17:09:55 -0300

    Plugin para speech bubbles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkSpeechBubble from './src/plugins/remark-speech-bubble/plugin';
 
 // https://astro.build/config
 export default defineConfig({
@@ -42,7 +43,13 @@ export default defineConfig({
 			customCss: [
 				'@fontsource-variable/inter',
 				'./src/styles/custom.css',
+				'./src/plugins/remark-speech-bubble/plugin.css',
 			]
 		}),
 	],
+	markdown: {
+		remarkPlugins: [
+			remarkSpeechBubble
+		]
+	}
 });
---
+++ src/plugins/remark-speech-bubble/plugin.js
@@ -0,0 +1,43 @@
+import { visit, SKIP } from "unist-util-visit";
+
+export default function remarkSpeechBubble() {
+    return function (tree) {
+        visit(tree, "listItem", (node) => {
+            if (
+                (node?.children?.at(0)?.type !== "paragraph")
+                || (node?.children?.at(0)?.children.at(0)?.type !== "text")
+                || (!node?.children?.at(0)?.children?.at(0)?.value)
+            ) {
+                return [SKIP];
+            }
+            const currentTextValue = node?.children?.at(0)?.children?.at(0)?.value;
+
+            const dataTaskMatch = currentTextValue.match(/\[(?<dataTask>\d)\]/);
+            if (dataTaskMatch === null) {
+                return [SKIP];
+            }
+            const dataTask = dataTaskMatch.groups.dataTask;
+
+            node.data = node?.data || {};
+            node.data.hProperties = node?.data?.hProperties || {};
+            node.data.hProperties.dataTask = node?.data?.hProperties?.dataTask || [];
+            node.data.hProperties.dataTask.push(dataTask);
+
+            node.children = [
+                {
+                    type: 'paragraph',
+                    children: currentTextValue.replace(/\[\d\] */g, "").split("\n").map(item => ({
+                        type: 'paragraph',
+                        children: [
+                            {
+                                type: 'text',
+                                value: item,
+                            }
+                        ],
+                    }))
+                }
+            ];
+            console.log(node.children.at(0));
+        });
+    };
+}
