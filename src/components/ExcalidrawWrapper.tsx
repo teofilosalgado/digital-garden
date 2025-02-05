27cd8028c8fa07857a4b6442eff251ba17ce7915
João Victor Teófilo Salgado <jvictorteo2000@hotmail.com>
2025-02-05 02:27:03 -0300

    Centralização automática.

--- src/components/ExcalidrawWrapper.tsx
+++ src/components/ExcalidrawWrapper.tsx
@@ -1,14 +1,41 @@
 'use client';
 import { Excalidraw } from '@excalidraw/excalidraw';
+import type { ExcalidrawImperativeAPI } from '@excalidraw/excalidraw/types/types';
+import { useEffect, useState } from 'react';
 
 interface ExcalidrawWrapperProps {
     initialData: any;
 }
 
 function ExcalidrawWrapper({ initialData }: ExcalidrawWrapperProps) {
+    const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI | null>(null);
+
+    console.log(document.documentElement.attributes.getNamedItem('data-theme')?.value);
+
+    useEffect(() => {
+        if (!excalidrawAPI || !initialData) {
+            return;
+        }
+
+        excalidrawAPI.scrollToContent(
+            excalidrawAPI.getSceneElements(),
+            {
+                fitToContent: true,
+                animate: true,
+                duration: 500,
+            }
+        );
+
+    }, [excalidrawAPI, initialData]);
+
     return (
         <div className='excalidraw-wrapper'>
-            <Excalidraw viewModeEnabled initialData={initialData} />
+            <Excalidraw
+                viewModeEnabled
+                excalidrawAPI={(api) => setExcalidrawAPI(api)}
+                initialData={initialData}
+                theme={document.documentElement.attributes.getNamedItem('data-theme')?.value || "dark"}
+            />
         </div>
     );
 };
